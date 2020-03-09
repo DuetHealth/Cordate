89098d0573792a0dd17042ff50225defcc04b2ee
kevin.li <kevin.li@duethealth.com>
2020-03-09 13:12:57 -0400

    Using carthage for dependencies

--- Cordate/Sources/Core/Cordate.h
+++
@@ -1,9 +0,0 @@
-@import UIKit;
-
-//! Project version number for Cordate.
-FOUNDATION_EXPORT double CordateVersionNumber;
-
-//! Project version string for Cordate.
-FOUNDATION_EXPORT const unsigned char CordateVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Cordate/PublicHeader.h>
