867b23c7f95403354724247783995479a42d7a20
kevin.li <kevin.li@duethealth.com>
2020-03-09 13:17:40 -0400

    Added package file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Cordate",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "Cordate",
+            targets: ["Cordate"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Cordate",
+            dependencies: ["RxSwift", "RxCocoa"],
+            path: "Cordate/Sources")
+    ]
+)
