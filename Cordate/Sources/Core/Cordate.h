646aa5df93a2756db44a307b0fffffbf6a410ed3
kevin.li <kevin.li@duethealth.com>
2020-03-11 11:05:15 -0400

    Added the header file back

---
+++ Cordate/Sources/Core/Cordate.h
@@ -0,0 +1,9 @@
+@import UIKit;
+
+//! Project version number for Cordate.
+FOUNDATION_EXPORT double CordateVersionNumber;
+
+//! Project version string for Cordate.
+FOUNDATION_EXPORT const unsigned char CordateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cordate/PublicHeader.h>
