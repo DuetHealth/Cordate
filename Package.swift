0703529e77a83fd9ef07e42d9fa310c43e2790e7
Keshav <keshavgn@gmail.com>
2022-10-21 22:27:47 +0530

    Updated RxSwift to 6.5.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Cordate"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("5.1.3"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0"))
     ],
     targets: [
         .target(
