10146c65f069dfd390db792d33917aa8075bbd8a
Venkata Krishna Garapati <gkrishnarayalu@gmail.com>
2026-04-24 12:16:17 -0400

    fix: use @unchecked Sendable for CalendarStyle (contains non-Sendable UIColor/UIFont)

--- Cordate/Sources/Core/Calendar/CalendarStyle.swift
+++ Cordate/Sources/Core/Calendar/CalendarStyle.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-public struct CalendarStyle: Sendable {
+public struct CalendarStyle: @unchecked Sendable {
 
     static func textColor(for color: UIColor) -> UIColor? {
         guard color != .clear else { return .black }
